7534e4f9987781f24cf84c98ae1d3855415bf98d
Nguyen Ngoc Phuong <nguyenngocphuongnb@gmail.com>
2024-12-01 22:53:53 +0700

    Revert "Revert "feat(auth): add missing local auth methods""

--- src/libs/auth/store.ts
+++ src/libs/auth/store.ts
@@ -15,6 +15,7 @@ export interface AuthStore {
   getDefaultKey(): string;
   setToken(token: string, key?: string): Promise<void>;
   getToken(key?: string): Promise<string | undefined>;
+  clearToken(key?: string): Promise<void>;
 }
 
 export class DefaultAuthStore {
@@ -39,10 +40,14 @@ export class DefaultAuthStore {
   getToken(key?: string): Promise<string | undefined> {
     return this.store.getToken(key);
   }
+
+  clearToken(key?: string): Promise<void> {
+    return this.store.clearToken(key);
+  }
 }
 
 export class BaseAuthStore {
-  constructor(protected defaultKey: string) { }
+  constructor(protected defaultKey: string) {}
 
   getDefaultKey(): string {
     return this.defaultKey;
@@ -61,6 +66,11 @@ export class BrowserStore extends BaseAuthStore {
   getToken(key?: string): Promise<string | undefined> {
     return Promise.resolve(this.cookie.get(key ?? this.defaultKey));
   }
+
+  clearToken(key?: string): Promise<void> {
+    this.cookie.remove(key ?? this.defaultKey);
+    return Promise.resolve();
+  }
 }
 
 // In node environment, we can't use cookies, so we use a simple in-memory store
@@ -76,5 +86,10 @@ export class NodeStore extends BaseAuthStore {
 
   getToken(key?: string): Promise<string | undefined> {
     return Promise.resolve(this.store[key ?? this.defaultKey]);
+  }
+
+  clearToken(key?: string): Promise<void> {
+    delete this.store[key ?? this.defaultKey];
+    return Promise.resolve();
   }
 }
